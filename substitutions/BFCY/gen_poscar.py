d5d9a052df8285a5c7c7bde66037a19d6b1c0466
leschultz <laneenriqueschultz@gmail.com>
2021-09-24 11:06:07 -0500

    Now doing sampling

--- substitutions/BFCY/gen_poscar.py
+++ substitutions/BFCY/gen_poscar.py
@@ -1,20 +1,23 @@
 from itertools import permutations
 from pymatgen import Structure
+
 import pandas as pd
 import numpy as np
 import random
+import os
 
 
 if __name__ == '__main__':
 
-    grab = 10
     swap = ['Y', 'Fe', 'Co', 'Zr']
     rem = ['O']
-    vo = 4
+    rem_num = 4
+    sample = 5
 
     structure = Structure.from_file('POSCAR')
     coords = structure.frac_coords
     species = list(map(str, structure.species))
+    lattice = structure.lattice
 
     df = pd.DataFrame(coords, columns=['x', 'y', 'z'])
     df['species'] = species
@@ -25,33 +28,26 @@ if __name__ == '__main__':
 
     # Get unique list of swaps
     swap_perm = set(permutations(swap['species'], swap.shape[0]))
-    rem_perm = [True]*(rem.shape[0]-vo)+[False]*(vo)
-    rem_perm = set(permutations(rem_perm, rem.shape[0]))
-    print(rem_perm)
+    rem_perm = set(permutations(rem.index, rem_num))
 
-    # Remove once every cite
-    rem_perm = []
-    cond = np.array([True]*rem.shape[0])
-    for i in range(rem.shape[0]):
-        indx = cond.copy()
-        indx[i] = False
-
-        rem_perm.append(indx)
+    # Sample randomly
+    swap_perm = random.sample(swap_perm, sample)
+    rem_perm = random.sample(rem_perm, sample)
 
     # Get every combination of swaps and removals
-    orders = []
-    for i in swap_perm:
+    count = 0
+    for i, j in zip(swap_perm, rem_perm):
         new_swap = swap.copy()
         new_swap['species'] = i
-        for j in rem_perm:
-            pos = pd.concat([keep, new_swap, rem[j]])
-            orders.append(pos)
+        new_rem = rem[~rem.index.isin(j)]
+        pos = pd.concat([keep, new_swap, new_rem])
 
-    # Grab random subset
-    random.shuffle(orders)
-    positions = orders[:grab]
+        structure = Structure(
+                              lattice,
+                              pos['species'],
+                              pos[['x', 'y', 'z']].values
+                              )
 
-    for i in positions:
-        print(i)
-
-    #structure.to(filename='POSCAR')
+        os.makedirs('{}'.format(count), exist_ok=True)
+        structure.to(filename='{}/POSCAR'.format(count))
+        count += 1
