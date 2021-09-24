a7fc0362e9511dbe9bded55ef9325199491b4a02
leschultz <laneenriqueschultz@gmail.com>
2021-09-24 15:01:30 -0500

    Corrected potentials order because POSCAR order changes once strcuctures are made

--- substitutions/BFCY/make_pot.sh
+++ substitutions/BFCY/make_pot.sh
@@ -1,5 +1,5 @@
 cat ~/potentials/vasp/pbe/Ba_sv/POTCAR >> POTCAR
-cat ~/potentials/vasp/pbe/Y_sv/POTCAR >> POTCAR
-cat ~/potentials/vasp/pbe/Fe_pv/POTCAR >> POTCAR
 cat ~/potentials/vasp/pbe/Co/POTCAR >> POTCAR
+cat ~/potentials/vasp/pbe/Fe_pv/POTCAR >> POTCAR
 cat ~/potentials/vasp/pbe/O/POTCAR >> POTCAR
+cat ~/potentials/vasp/pbe/Y_sv/POTCAR >> POTCAR
